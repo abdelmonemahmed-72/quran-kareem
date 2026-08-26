5952ce9ec48f90f3ab989f87e9b123af4332796a
abdelmonemahmed-72 <elhagienabdoahmed@gmail.com>
2026-08-26 10:45:02 +0300

    Fix Capacitor permissions

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/quran-kareem/sw.js', { scope: '/quran-kareem/' })})}
